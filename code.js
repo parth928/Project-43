addf3b64c6fb9ff4317d021a4487a1128d387d11
parth928 <75481435+parth928@users.noreply.github.com>
2021-08-07 18:48:51 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,21 @@
+const accessCode1="VARIABLE";
+const accessCode2="FUNCTION";
+const accessCode3="DATABASE";
+function clues(){
+    fill("white");
+    textSize(15);
+    text("REVBAILA",100,50);
+    fill("lightblue");
+text("Hint : Always changing, notConstant",100,70);
+fill("white");
+    textSize(15);
+    text("CUTNIFON",700,150);
+    fill("lightblue");
+text("Hint : Performs a particular task",700,70);
+fill("white");
+    textSize(15);
+    text("ATEDASBA",100,250);
+    fill("lightblue");
+text("Hint : Store All Information",100,270);
+}
+
